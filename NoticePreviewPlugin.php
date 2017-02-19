7392086a98e144e71406f4ebd7780b4b1dcad5d9
Chimo <chimo@chromic.org>
2017-02-18 19:25:09 -0500

    Tabs to spaces

--- NoticePreviewPlugin.php
+++ NoticePreviewPlugin.php
@@ -8,63 +8,63 @@ class NoticePreviewPlugin extends Plugin
 {
     const VERSION = '0.0.1';
 
-	function onRouterInitialized($m)
-	{
-		$m->connect(
-			'main/render-notice', array(
-					'action' => 'rendernotice'
-				)
-		);
+    function onRouterInitialized($m)
+    {
+        $m->connect(
+            'main/render-notice', array(
+                    'action' => 'rendernotice'
+                )
+        );
 
-		return true;
-	}
+        return true;
+    }
 
     function onStartShowNoticeFormData($action)
     {
-		$action->elementStart('div', array('id' => 'chr-notice-preview-container'));
+        $action->elementStart('div', array('id' => 'chr-notice-preview-container'));
 
-		$action->elementStart('ul');
+        $action->elementStart('ul');
 
-		$action->elementStart('li');
-		$action->element('a', array('href' => '#chr-notice-compose'), 'Compose');
-		$action->elementEnd('li');
+        $action->elementStart('li');
+        $action->element('a', array('href' => '#chr-notice-compose'), 'Compose');
+        $action->elementEnd('li');
 
-		$action->elementStart('li');
-		$action->element('a', array('href' => '#chr-notice-preview'), 'Preview');
-		$action->elementEnd('li');
+        $action->elementStart('li');
+        $action->element('a', array('href' => '#chr-notice-preview'), 'Preview');
+        $action->elementEnd('li');
 
-		$action->elementEnd('ul');
+        $action->elementEnd('ul');
 
-		$action->elementStart('div', array('id' => 'chr-notice-compose'));
+        $action->elementStart('div', array('id' => 'chr-notice-compose'));
 
-		return true;
+        return true;
     }
 
-	function onEndShowNoticeFormData($action)
-	{
-		// Close the 'Compose' container
-		$action->elementEnd('div');
+    function onEndShowNoticeFormData($action)
+    {
+        // Close the 'Compose' container
+        $action->elementEnd('div');
 
-		// The 'Preview' container
-		$action->elementStart('div', array('id' => 'chr-notice-preview'));
-		// TODO: Get the content from the notice textbox and render it.
-		$action->elementEnd('div');
+        // The 'Preview' container
+        $action->elementStart('div', array('id' => 'chr-notice-preview'));
+        // TODO: Get the content from the notice textbox and render it.
+        $action->elementEnd('div');
 
-		// Close the 'tabs' container
-		$action->elementEnd('div');
+        // Close the 'tabs' container
+        $action->elementEnd('div');
 
-		return true;
-	}
+        return true;
+    }
 
-	function onEndShowStyles($action)
-	{
-		$action->cssLink($this->path('css/chr-notice-preview.css'));
-	}
+    function onEndShowStyles($action)
+    {
+        $action->cssLink($this->path('css/chr-notice-preview.css'));
+    }
 
-	function onEndShowScripts($action)
-	{
-		$action->script($this->path('js/chr-notice-preview.js'));
-	}
+    function onEndShowScripts($action)
+    {
+        $action->script($this->path('js/chr-notice-preview.js'));
+    }
 
     function onPluginVersion(array &$versions)
     {
